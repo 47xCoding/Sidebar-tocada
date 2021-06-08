e5b2932260a03ba46fef5e2516960dc0e29b4599
47xCoding <74483645+47xCoding@users.noreply.github.com>
2021-06-08 12:39:29 +0600

    Added necessary scripts.
    Added scripts to toggle button to toggle the sidebar and scripts to close button to close the sidebar.

---
+++ setup/app.js
@@ -0,0 +1,12 @@
+// select items
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+// add event 
+toggleBtn.addEventListener('click', function () {
+ sidebar.classList.toggle("show-sidebar");
+})
+closeBtn.addEventListener('click', function () {
+ sidebar.classList.remove('show-sidebar')
+})
